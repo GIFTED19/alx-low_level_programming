d2e61e23203e198f9b9330f737fc023937eb75f8
Neo <gifted.colosseum@outlook.com>
2023-07-14 03:28:55 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
+ * main - printing without printf and puts
  *
- * Return: Always 0 (Success)
+ * Return:  always (1)
+ *
  */
 int main(void)
 {
-	printf("Size of a char: %d byte(s)\n", sizeof(char));
-	printf("Size of an int: %d byte(s)\n", sizeof(int));
-	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %d byte(s)\n" sizeof(long long int));
-	printf("Size of a float: %d byte(s)\n" sizeof(float));
-	return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	return (1);
 }
